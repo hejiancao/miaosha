f67ad6bf24f0edb1f2e8253a6ae4ab219b31bc3e
sen.shao <shaos91@126.com>
2020-02-28 09:18:05 +0800

    修改交换机名称

--- src/main/java/com/imooc/miaosha/rabbitmq/MQConfig.java
+++ src/main/java/com/imooc/miaosha/rabbitmq/MQConfig.java
@@ -16,9 +16,9 @@ public class MQConfig {
     public static final String TOPIC_QUEUE1 = "topic.queue1";
     public static final String TOPIC_QUEUE2 = "topic.queue2";
     public static final String HEADER_QUEUE = "header.queue";
-    public static final String TOPIC_EXCHANGE = "topicExchage";
-    public static final String FANOUT_EXCHANGE = "fanoutxchage";
-    public static final String HEADERS_EXCHANGE = "headersExchage";
+    public static final String TOPIC_EXCHANGE = "topicExchange";
+    public static final String FANOUT_EXCHANGE = "fanoutExchange";
+    public static final String HEADERS_EXCHANGE = "headersExchange";
 
     /**
      * Direct模式 交换机Exchange
